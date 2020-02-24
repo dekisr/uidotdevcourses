aa56be0a61651afea4c219db6118d69ad5f26bfe
Hideki J <aledinai@gmail.com>
2020-02-24 12:04:00 -0300

    Update github-battle - Navbar State

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,16 +1,41 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage })
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
-      <ul className="flex-center">
-        {languages.map((language) => (
-          <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <ul className="flex-center">
+          {languages.map((language) => (
+            <li key={language}>
+              <button
+                className="btn-clear nav-link"
+                style={
+                  language === this.state.selectedLanguage
+                    ? { color: 'tomato' }
+                    : null
+                }
+                onClick={() => this.updateLanguage(language)}
+              >
+                {language}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {this.state.selectedLanguage}
+      </React.Fragment>
     )
   }
 }
