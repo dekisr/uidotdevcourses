de9d0303c4b39cbd48d121303d47947ea0ff6d31
Hideki J <aledinai@gmail.com>
2020-02-27 08:07:57 -0300

    Update github-battle - Toggle Theme

---
+++ github-battle/app/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ThemeConsumer } from '../contexts/theme'
+
+export default function Nav() {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className="row space-between">
+          <button
+            style={{ fontSize: 30 }}
+            onClick={toggleTheme}
+            className="btn-clear"
+          >
+            {theme === 'light' ? '🔦' : '💡'}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  )
+}
