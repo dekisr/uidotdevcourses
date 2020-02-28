f3d3bdeaed96ded7b5c4a43cc028d2063e5ab3be
Hideki J <aledinai@gmail.com>
2020-02-28 06:17:54 -0300

    Update github-battle - Query Strings

--- github-battle/app/components/Battle.js
+++ github-battle/app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Results from './Results'
 import { ThemeConsumer } from '../contexts/theme'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {
   FaUserFriends,
@@ -139,8 +140,7 @@ export default class Battle extends React.Component {
 
     this.state = {
       playerOne: null,
-      playerTwo: null,
-      battle: false
+      playerTwo: null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -158,22 +158,7 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo, battle } = this.state
-    if (battle === true) {
-      return (
-        <Results
-          playerOne={playerOne}
-          playerTwo={playerTwo}
-          onReset={() =>
-            this.setState({
-              playerOne: null,
-              playerTwo: null,
-              battle: false
-            })
-          }
-        />
-      )
-    }
+    const { playerOne, playerTwo } = this.state
     return (
       <React.Fragment>
         <Instructions />
@@ -206,12 +191,15 @@ export default class Battle extends React.Component {
             )}
           </div>
           {playerOne && playerTwo && (
-            <button
+            <Link
               className="btn dark-btn btn-space"
-              onClick={() => this.setState({ battle: true })}
+              to={{
+                pathname: '/battle/results',
+                search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+              }}
             >
               Battle
-            </button>
+            </Link>
           )}
         </div>
       </React.Fragment>
--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -3,6 +3,8 @@ import Card from './Card'
 import Tooltip from './Tooltip'
 import Loading from './Loading'
 import PropTypes from 'prop-types'
+import queryString from 'query-string'
+import { Link } from 'react-router-dom'
 import { battle } from '../utils/api'
 import {
   FaCompass,
@@ -62,7 +64,9 @@ export default class Results extends React.Component {
     }
   }
   componentDidMount() {
-    const { playerOne, playerTwo } = this.props
+    const { playerOne, playerTwo } = queryString.parse(
+      this.props.location.search
+    )
     battle([playerOne, playerTwo])
       .then((players) => {
         this.setState({
@@ -109,15 +113,15 @@ export default class Results extends React.Component {
             <ProfileList profile={loser.profile} />
           </Card>
         </div>
-        <button onClick={this.props.onReset} className="btn dark-btn btn-space">
+        <Link to="/battle" className="btn dark-btn btn-space">
           Reset
-        </button>
+        </Link>
       </React.Fragment>
     )
   }
 }
-Results.propTypes = {
-  playerOne: PropTypes.string.isRequired,
-  playerTwo: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
+// Results.propTypes = {
+//   playerOne: PropTypes.string.isRequired,
+//   playerTwo: PropTypes.string.isRequired,
+//   onReset: PropTypes.func.isRequired
+// }
