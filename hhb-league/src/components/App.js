b6d1bc91a84785c8090267d4798e427c491fd0f5
Hideki J <aledinai@gmail.com>
2020-03-12 16:30:54 -0300

    Update hhb-league - Set up the initial routes / Add CSS

--- hhb-league/src/components/App.js
+++ hhb-league/src/components/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+import Players from './Players'
+import Teams from './Teams'
+import Navbar from './Navbar'
 
-function App() {
-  return <div>React Router Course</div>
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+        </div>
+      </Router>
+    )
+  }
 }
-
-export default App
