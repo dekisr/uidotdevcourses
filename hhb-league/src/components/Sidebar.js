9d67cc70ccfc6573e9e0621da6f4c24516186657
Hideki J <aledinai@gmail.com>
2020-03-12 17:35:08 -0300

    Update hhb-league - Fetch the players

---
+++ hhb-league/src/components/Sidebar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import slug from 'slug'
+
+export default function Sidebar({ title, list, loading, location, match }) {
+  // prettier-ignore
+  return loading === true
+  ? <h1>LOADING</h1>
+  : <div>
+      <h3 className="header">{title}</h3>
+      <ul className="sidebard-list">
+        {list.map((item) => (
+          <CustomLink
+            key={item}
+            to={{
+              pathname: `${match.url}/${slug(item)}`,
+              search: location.search
+            }}>
+            {item.toUpperCase()}
+          </CustomLink>
+        ))}
+      </ul>
+    </div>
+}
+
+function CustomLink({ to, children }) {
+  return (
+    <Route
+      path={to.pathname}
+      children={({ match }) => (
+        <li
+          style={{
+            listStyleType: 'none',
+            fontWeight: match ? 'bold' : 'normal'
+          }}
+        >
+          <Link to={to}>{children}</Link>
+        </li>
+      )}
+    />
+  )
+}
+
+Sidebar.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+}
