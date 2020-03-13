cdaf276ba1294cd28908720ace4307b8aa51433b
Hideki J <aledinai@gmail.com>
2020-03-13 00:04:21 -0300

    Update hhb-league - Fetch the teams

--- hhb-league/src/components/Teams.js
+++ hhb-league/src/components/Teams.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
 
 export default class Teams extends Component {
+  state = {
+    teamNames: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    getTeamNames().then((teamNames) => {
+      this.setState({
+        loading: false,
+        teamNames
+      })
+    })
+  }
   render() {
-    return <div className="container two-column">Teams</div>
+    const { loading, teamNames } = this.state
+    const { location, match } = this.props
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Teams"
+          list={teamNames}
+          {...this.props}
+        />
+        {!loading && location.pathname === '/teams' && (
+          <div className="sidebar-instruction">Select a Team</div>
+        )}
+      </div>
+    )
   }
 }
