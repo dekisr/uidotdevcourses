934ee14fe401a3a216e66521129111e2eee65965
Hideki J <aledinai@gmail.com>
2020-08-15 04:40:03 -0300

    hhb-league-rrv5 - Home Route

--- hhb-league-rrv5/src/components/Home.js
+++ hhb-league-rrv5/src/components/Home.js
@@ -1,9 +1,22 @@
 import * as React from 'react'
+import TeamLogo from './TeamLogo'
+import { Link } from 'react-router-dom'
+import useTeamNames from '../hooks/useTeamNames'
 
 const Home = () => {
-  return (
+  const { loading, response: teamNames } = useTeamNames()
+
+  return loading ? null : (
     <div className="container">
-      Home
+      <h1 className="large-header">Hash History Basketball League</h1>
+      <h3 className="header text-center">Select a Team</h3>
+      <div className="home-grid">
+        {teamNames.map((id) => (
+          <Link key={id} to={`/${id}`}>
+            <TeamLogo id={id} width="125px" />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
